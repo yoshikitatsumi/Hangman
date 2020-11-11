6dce4032a60eb05a125ebdbefcb00da75b483c9e
cdup <cdup@MSI>
2020-11-12 11:34:50 +1300

    Add project files.

---
+++ Hangman/Hangman/HangManModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace Hangman
+{
+    public class HangmanModel : INotifyPropertyChanged
+    {
+        public HangmanModel()
+        {
+        }
+
+        public int Id { get; set; }
+        public string NameOfPlayer { get; set; }
+        public string Difficulty { get; set; }
+        public string StateOfGame { get; set; }
+        public int Score { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
