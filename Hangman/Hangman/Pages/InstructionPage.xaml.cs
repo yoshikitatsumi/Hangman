2b25059ec54e9a2fb22c2106e633217363aa21b2
cdup <cdup@MSI>
2020-11-20 09:26:43 +1300

    Fix errors: about the namespaces

--- Hangman/Hangman/Pages/InstructionPage.xaml.cs
+++ Hangman/Hangman/Pages/InstructionPage.xaml.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace AndroidHangman.Pages
+namespace Hangman.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class InstructionPage : ContentPage
