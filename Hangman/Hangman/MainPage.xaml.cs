632da23f26d2166e7bfdc8d09150ceb4e2ccac28
Yoshi Tatsumi <YT007@atcltd.atc.org.nz>
2020-11-19 15:52:48 +1300

    Yoshi

--- Hangman/Hangman/MainPage.xaml.cs
+++ Hangman/Hangman/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AndroidHangman.Pages;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -6,13 +7,104 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace Hangman
+namespace AndroidHangman
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            Label Welcome = new Label
+            {
+                Text = "Welcome to Hangman game!",
+                TextColor = Color.Blue,
+                FontSize = 25
+            };
+            Button btnGame = new Button
+            {
+                Text = "Start the Game!",
+                FontSize = 25,
+                TextColor = Color.Red
+            };
+            btnGame.Clicked += StartGame_Clicked;
+
+            Button btnInst = new Button
+            {
+                Text = "Instructions",
+                FontSize = 25,
+                TextColor = Color.Green
+            };
+            btnInst.Clicked += Instruction_Clicked;
+            Button btnProf = new Button
+            {
+                Text = "Profile",
+                FontSize = 25,
+                TextColor = Color.Yellow
+            };
+            btnProf.Clicked += Profile_Clicked;
+            Button btnDB = new Button
+            {
+                Text = "DB / Extra",
+                FontSize = 25,
+                TextColor = Color.Gray
+            };
+            btnDB.Clicked += DB_Clicked;
+            Button btnExit = new Button
+            {
+                Text = "Exit",
+                FontSize = 25,
+                TextColor = Color.Black
+            };
+            Content = new StackLayout
+            {
+                Children =
+            {
+                Welcome,
+                new StackLayout
+                {
+                    HorizontalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        btnGame,
+                        btnInst,
+                        btnProf,
+                        btnDB,
+                        btnExit,
+                    }
+
+
+                }
+
+            }
+
+            };
         }
+        private void Instruction_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new InstructionPage());
+        }
+
+        private void Profile_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ProfilePage());
+        }
+        private void DB_Clicked(object sender, EventArgs e)
+        {
+            //Navigation.PushAsync(new DBPage());
+        }
+
+        //private void Exit_Clicked(object sender, EventArgs e)
+        //{
+        //    Navigation.PushAsync(new ExitPage());
+        //}
+
+        public void StartGame_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LevelPage());
+        }
+
+
+
     }
 }
--- Hangman/Hangman/Pages/ProfilePage.xaml.cs
+++ Hangman/Hangman/Pages/ProfilePage.xaml.cs
@@ -9,31 +9,92 @@ using Xamarin.Forms.Xaml;
 
 namespace AndroidHangman.Pages
 {
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ProfilePage : ContentPage
     {
+
+        public int gender = 1;
+        Image avatar = new Image
+        {
+
+            Source = "/Resources/drawable/Avatar" + 1 + ".jpg"
+        };
+        Entry PName = new Entry
+        {
+            Text = "",
+            FontSize = 15,
+            TextColor = Color.DarkBlue
+        };
+        Entry UName = new Entry
+        {
+            Text = "",
+            FontSize = 15,
+            TextColor = Color.DarkBlue
+        };
+        Label UScore = new Label
+        {
+            Text = "Score: 0",
+            FontSize = 20,
+            TextColor = Color.Orange
+    };
+        Label UGems = new Label
+        {
+            Text = "Gems: 0",
+            FontSize = 20,
+            TextColor = Color.Green
+        };
+        Button btnSave = new Button
+        {
+            Text = "Save",
+            TextColor = Color.Blue
+        };
+        Button btnDelete = new Button
+        {
+            Text = "Delete",
+            TextColor = Color.Red
+        };
+
+
         public ProfilePage()
         {
             InitializeComponent();
 
-        }
-        private void Edit_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new InstructionPage());
-        }
+            Content = new StackLayout
+            {
+                Children =
+            {
+                avatar,
+                new StackLayout
+                {
+                    HorizontalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        PName,
+                        UName,
+                        UScore,
+                        UGems,
+                        btnSave,
+                        btnDelete,
+                    }
+                    
 
-        private void Delete_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new ProfilePage());
-        }
-        private void Create_Clicked(object sender, EventArgs e)
-        {
-            
-        }
+                }
 
-        private void Avatar_Clicked(object sender, EventArgs e)
-        {
-            
+            }
+
+            };
+
         }
+        //private void Save_Clicked(object sender, EventArgs e)
+        //{
+        //    Navigation.PushAsync(new SavePage());
+        //}
+
+        //private void Delete_Clicked(object sender, EventArgs e)
+        //{
+        //    Navigation.PushAsync(new DeletePage());
+        //}
+
     }
 }
